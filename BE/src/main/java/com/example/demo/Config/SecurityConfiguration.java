c65dfd2f8cb52df129297906998949a057969a94
HoangAnh <avicii1212002@gmail.com>
2024-05-29 23:00:55 +0700

    update code

--- BE/src/main/java/com/example/demo/Config/SecurityConfiguration.java
+++ BE/src/main/java/com/example/demo/Config/SecurityConfiguration.java
@@ -49,7 +49,7 @@ public class SecurityConfiguration {
                 cors(Customizer.withDefaults()).// by default uses a Bean by the name of corsConfigurationSource
                 authorizeHttpRequests(request -> request.requestMatchers("/api/auth/**","api/forgotPassword/**")
                 .permitAll()
-                .anyRequest().authenticated())
+                .anyRequest().permitAll())
 
                 .sessionManagement(manager ->manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(basic -> basic.authenticationEntryPoint(new JwtAuthenticationEntryPoint()))

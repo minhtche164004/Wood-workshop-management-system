78ea0af03bcbb30291f38c3e56901de54fa0c80a
namlhe161914 <namlhe161914@fpt.edu.vn>
2024-06-09 15:51:26 +0700

    update transactions

--- BE/src/main/java/com/example/demo/Controllers/PaymentController.java
+++ BE/src/main/java/com/example/demo/Controllers/PaymentController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controllers;
 import com.example.demo.Config.VNPayService;
 import com.example.demo.Service.Impl.PaymentServiceImpl;
 import com.example.demo.Service.PaymentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import net.minidev.json.JSONArray;
@@ -11,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @org.springframework.stereotype.Controller
 
@@ -25,22 +29,22 @@ public class PaymentController {
 
 
     @GetMapping("")
-    public String home(){
+    public String home() {
         return "src/main/resources/templates/index.html";
     }
 
     @PostMapping("/submitOrder")
     public String submidOrder(@RequestParam("amount") int orderTotal,
-                            @RequestParam("orderInfo") String orderInfo,
-                            HttpServletRequest request){
+                              @RequestParam("orderInfo") String orderInfo,
+                              HttpServletRequest request) {
         String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
         String vnpayUrl = vnPayService.createOrder(orderTotal, orderInfo, baseUrl);
         return "redirect:" + vnpayUrl;
     }
 
     @GetMapping("/vnpay-payment")
-    public String GetMapping(HttpServletRequest request, Model model){
-        int paymentStatus =vnPayService.orderReturn(request);
+    public String GetMapping(HttpServletRequest request, Model model) {
+        int paymentStatus = vnPayService.orderReturn(request);
 
         String orderInfo = request.getParameter("vnp_OrderInfo");
         String paymentTime = request.getParameter("vnp_PayDate");
@@ -63,10 +67,24 @@ public class PaymentController {
 //        return ResponseEntity.ok(token);
 //    }
 
-    @GetMapping("/getTransaction")
-    public ResponseEntity<String> getTransactions() {
+//    @GetMapping("/getTransaction")
+//    public ResponseEntity<String> getTransactions() {
+//
+//        String info = String.valueOf(paymentService.getHistoriesTransactions());
+//        return ResponseEntity.ok(info);
+//    }
 
-        String info = String.valueOf(paymentService.getHistoriesTransactions());
-        return ResponseEntity.ok(info);
+
+    @GetMapping("/getTransaction")
+    public ResponseEntity<Map> getTransactions() {
+        String info = paymentService.getHistoriesTransactions();
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = new HashMap<>();
+        try {
+            map = mapper.readValue(info, Map.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(map);
     }
 }

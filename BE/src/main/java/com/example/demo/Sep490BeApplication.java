e6ffb986e8f41b3f277c6417dfd6e20e2e591613
Minh <minhtche164004@fpt.edu.vn>
2024-06-21 00:17:33 +0700

    test

--- BE/src/main/java/com/example/demo/Sep490BeApplication.java
+++ BE/src/main/java/com/example/demo/Sep490BeApplication.java
@@ -11,7 +11,7 @@ public class Sep490BeApplication {
 
 	public static void main(String[] args) {
 		Jedis jedis = new Jedis("redis://default:xcHV2iYQow6KxZMxZz4klIoKxgYuytAf@redis-17341.c251.east-us-mz.azure.redns.redis-cloud.com:17341");
-		jedis.setex("minh",60,"leaddzvl");
+		jedis.setex("minh",60,"leaddzv");
 		System.out.println(jedis.get("minh"));
 		SpringApplication.run(Sep490BeApplication.class, args);
 	}

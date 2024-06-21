f860c3eea0eccf372455a0e4314edd538077e697
Minh <minhtche164004@fpt.edu.vn>
2024-06-21 20:49:51 +0700

    fix

--- FE/SEP490/src/app/environments/environment.prod.ts
+++ FE/SEP490/src/app/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://dogosydungbe.azurewebsites.net/' // Thay đổi URL tại đây
+  // apiUrl: 'https://dogosydungbe.azurewebsites.net/' // URL server
+  apiUrl: 'http://localhost:8080/' // URL local
 };
--- FE/SEP490/src/app/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    apiUrl: '${environment.apiUrl}' // URL API cho môi trường development
-  };

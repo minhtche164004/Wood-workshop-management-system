29a9fdecb6fab2e2e93a424212eb5346745a8b6b
HoangAnh <avicii1212002@gmail.com>
2024-06-29 17:54:11 +0700

    update

--- BE/src/main/java/com/example/demo/Controllers/SubMaterialController/SubMaterialController.java
+++ BE/src/main/java/com/example/demo/Controllers/SubMaterialController/SubMaterialController.java
@@ -146,21 +146,23 @@ public class SubMaterialController {
     }
 
     @PostMapping("/createExportMaterialProductTotalJob")
-    public ResponseEntity<ApiResponse<List<String>>> createExportMaterialProductTotalJob(@RequestParam("id") int id ,@RequestParam("mate_id") int mate_id, @RequestBody QuantityTotalDTO quantityTotalDTO) {
-        return subMaterialService.createExportMaterialProductTotalJob(id,mate_id,quantityTotalDTO);
+    public ResponseEntity<ApiResponse<List<String>>> createExportMaterialProductTotalJob(@RequestParam("id") int id ,@RequestParam("mate_id") int mate_id, @RequestBody QuantityTotalDTO quantityTotalDTO
+    ,@RequestParam("emp_id") int emp_id) {
+        return subMaterialService.createExportMaterialProductTotalJob(id,mate_id,quantityTotalDTO,emp_id);
     }
 
     @PostMapping("/createExportMaterialRequestTotalJob")
-    public ResponseEntity<ApiResponse<List<String>>> createExportMaterialRequestTotalJob(@RequestParam("id") int id ,@RequestParam("mate_id") int mate_id, @RequestBody QuantityTotalDTO quantityTotalDTO) {
-        return subMaterialService.createExportMaterialRequestTotalJob(id,mate_id,quantityTotalDTO);
+    public ResponseEntity<ApiResponse<List<String>>> createExportMaterialRequestTotalJob(@RequestParam("id") int id ,@RequestParam("mate_id") int mate_id, @RequestBody QuantityTotalDTO quantityTotalDTO
+            ,@RequestParam("emp_id") int emp_id) {
+        return subMaterialService.createExportMaterialRequestTotalJob(id,mate_id,quantityTotalDTO,emp_id);
     }
 
-    @PostMapping("/CreateEMaterial")
-    public ApiResponse<?> CreateEMaterial(@RequestParam("product_id") int product_id ,@RequestParam("mate_id") int mate_id,@RequestParam("emp_id") int emp_id) {
-        ApiResponse<List> apiResponse = new ApiResponse<>();
-        apiResponse.setResult(subMaterialService.createEMaterial(emp_id, mate_id,product_id));
-        return apiResponse;
-    }
+//    @PostMapping("/CreateEMaterial")
+//    public ApiResponse<?> CreateEMaterial(@RequestParam("product_id") int product_id ,@RequestParam("mate_id") int mate_id,@RequestParam("emp_id") int emp_id) {
+//        ApiResponse<List> apiResponse = new ApiResponse<>();
+//        apiResponse.setResult(subMaterialService.createEMaterial(emp_id, mate_id,product_id));
+//        return apiResponse;
+//    }
     @GetMapping("/getAllEmpMate")
     public ApiResponse<?> getAllEmpMate() {
         ApiResponse<List> apiResponse = new ApiResponse<>();

c49c4c4f2c6e2416b6f4935f452e2f0e512f01b0
HoangAnh <avicii1212002@gmail.com>
2024-07-02 17:16:32 +0700

    update code ngày 2/7

--- BE/src/main/java/com/example/demo/Dto/OrderDTO/RequestOrder.java
+++ BE/src/main/java/com/example/demo/Dto/OrderDTO/RequestOrder.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -13,5 +15,6 @@ public class RequestOrder {
     private ReceiveInfo receiveInfo;
     private OrderDetail orderDetail;
     private Integer payment_method;
+    private Date orderFinish;
 
 }

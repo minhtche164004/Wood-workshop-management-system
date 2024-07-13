936bf6658ccee8235af5fefb2a3c807611938f87
Minh <minhtche164004@fpt.edu.vn>
2024-07-13 23:11:43 +0700

    fix get redis instance

--- BE/src/main/java/com/example/demo/Config/RedisConfig.java
+++ BE/src/main/java/com/example/demo/Config/RedisConfig.java
@@ -10,10 +10,7 @@ public class RedisConfig {
 
     private volatile static JedisPooled redisInstance;
     public synchronized static JedisPooled getRedisInstance() {
-        if(redisInstance != null){
-            return redisInstance;
-        }
-        else {
+        if(redisInstance == null) {
             HostAndPort address = new HostAndPort("redis-17341.c251.east-us-mz.azure.redns.redis-cloud.com", 17341);
             JedisClientConfig config = DefaultJedisClientConfig.builder()
                     .user("default")

514c92f14954ecebab6cfdfc840b2cc35dcd206d
anhvubui382 <116990776+anhvubui382@users.noreply.github.com>
2024-07-27 23:09:34 +0700

    update

--- FE/SEP490/src/app/app-routing.module.ts
+++ FE/SEP490/src/app/app-routing.module.ts
@@ -67,6 +67,7 @@ import { EmployeeSubmaterialManagementComponent } from './employee-submaterial-m
 import { ImpactHistoryManagerComponent } from './3.Manager/impact-history-manager/impact-history-manager.component';
 
 
+
 const routes: Routes = [
   { path: 'homepage', component: HomepageComponent },
   { path: 'api-province', component: ApiProvinceComponent },
@@ -112,12 +113,9 @@ const routes: Routes = [
   { path: 'employee-job', component: EmployeeJobComponent },
   { path: 'bill', component: BillComponent },
   { path: 'product-details/:id', component: ProductDetailComponent },
-
   { path: 'wishlist', component: WishlistComponent },
   { path: 'template', component: TemplateComponent },
-
   { path: 'ds', component: DashboardComponent },
-
   { path: 'static-report', component: ChartComponent, canActivate: [AuthGuard], data: { roles: ['MANAGER'] } },
   { path: 'manager', component: ChartComponent, canActivate: [AuthGuard], data: { roles: ['MANAGER'] } },
   { path: 'material_management', component: MaterialManagementComponent, canActivate: [AuthGuard], data: { roles: ['MANAGER'] } },
@@ -129,12 +127,12 @@ const routes: Routes = [
   { path: 'autoComplete', component: AutoCompleteComponent },
   { path: 'history_order', component: HistoryOrderComponent },
   { path: 'request_product', component: ListRequestProductComponent },
-
   { path: 'employee', component: ListJobDoneEmployeeComponent, canActivate: [AuthGuard], data: { roles: ['EMPLOYEE'] } },
   { path: 'list_job_done', component: ListJobDoneComponent, canActivate: [AuthGuard], data: { roles: ['ADMIN'] } },
   { path: 'list_request_customer', component: ListRequestCustomerComponent, canActivate: [AuthGuard], data: { roles: ['MANAGER'] } },
   { path: 'list_salary_employee', component: ListSalaryEmployeeComponent, canActivate: [AuthGuard], data: { roles: ['EMPLOYEE'] } },
   { path: 'temperlate', component: TemplateComponent },
+
   //danh` cho order-vnpay
   { path: 'order-vnpay-success', component: OrderSuccessComponent },
   { path: 'order-vnpay-fail', component: OrderFailComponent },

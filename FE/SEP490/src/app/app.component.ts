18efe737d8e65d01656c2606a5f41cf52c783a61
namlhe161914 <namlhe161914@fpt.edu.vn>
2024-08-01 03:06:00 +0700

    update tinh nang load sang compoment khac bi delay (da fix)

--- FE/SEP490/src/app/app.component.ts
+++ FE/SEP490/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core'
 import { Chart, registerables } from 'chart.js';
+import { AuthGuard } from './AuthGuard';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoadingService } from './LoadingService';
+
 Chart.register(...registerables)
 @Component({
   selector: 'app-root',
@@ -8,5 +13,10 @@ Chart.register(...registerables)
 })
 export class AppComponent {
   title = 'SEP490';
-  
+  isLoading$ = this.loadingService.loading$;
+
+  constructor(private loadingService: LoadingService) { }
+  ngOnInit() {
+
+  }
 }
